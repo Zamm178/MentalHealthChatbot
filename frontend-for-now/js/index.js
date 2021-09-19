d758075421bd9e24f35154402013acb84e380347
Muhammad Talha <sulemanshah432@gmail.com>
2021-09-19 19:38:10 +0530

    front end

--- frontend-for-now/js/index.js
+++ frontend-for-now/js/index.js
@@ -1,3 +1,5 @@
+var $symptomsArray = [];
+
 var $messages = $('.messages-content'),
     d, h, m,
     i = 0;
@@ -29,6 +31,25 @@ function insertMessage() {
     if ($.trim(msg) == '') {
         return false;
     }
+
+    $symptomsArray.push(msg);
+    console.log($symptomsArray);
+
+    if ($symptomsArray.length === Fake.length - 12) {
+        var symptoms = $symptomsArray;
+      $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3000/api/reply?symptoms='+JSON.stringify(symptoms),
+        success: function(data){
+            if(data[0]!==null){
+                Fake[i] = Fake[i+1] + data[0];
+                fakeMessage();
+            }
+        }
+      });
+      return false;
+    }
+
     $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
     setDate();
     $('.message-input').val(null);
@@ -49,30 +70,50 @@ $(window).on('keydown', function(e) {
     }
 })
 
+// var Fake = [
+//     'Hi there, I\'m Chatbot and you?',
+//     'Is there anything I can help you with?',
+//     'Nice to meet you',
+//     'How are you?',
+//     'That\'s awesome',
+//     'Why do you think that?',
+//     'Can you explain?',
+//     'Anyway I\'ve gotta go now',
+//     'It was a pleasure chat with you',
+//     'Bye',
+//     ':)'
+// ]
+
 var Fake = [
-    'Hi there, I\'m Chatbot and you?',
-    'Is there anything I can help you with?',
-    'Nice to meet you',
-    'How are you?',
-    'That\'s awesome',
-    'Why do you think that?',
-    'Can you explain?',
-    'Anyway I\'ve gotta go now',
-    'It was a pleasure chat with you',
-    'Bye',
-    ':)'
+	"Hey there, User",
+	"I'm looking forward to getting to know you better",
+	"This is where we can chat",
+	"Before we get into that, I want to get sense of you've been feeling lately",
+	"I've got a few questions to ask you",
+	"Now let's move on to the good stuff",
+	"So, where should we start?", // 1
+	"Of course",
+	"What did you want to talk about today?", // 2
+	"Will you tell me a bit about what's going on?", // 3
+	"So tell me, how are you feeling today?", // 4
+	"What hasn't been going well at the moment?",
+	"Keeping it short and specific, type your first one here", // 5
+	"Were you bothered by not being able to stop or control worry?", // 6 - over-activity
+	"Have you been bothered by little interest or pleasure in doing things?", // 7 - loss of interest
+	`I'm sorry that you're going through this, I strongly recommend that you
+	go to any website here: `
 ]
 
 function fakeMessage() {
     if ($('.message-input').val() != '') {
         return false;
     }
-    $('<div class="message loading new"><figure class="avatar"><img src="img/.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+    $('<div class="message loading new"><figure class="avatar"><img src="img/img.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
     updateScrollbar();
 
     setTimeout(function() {
         $('.message.loading').remove();
-        $('<div class="message new"><figure class="avatar"><img src="img/.png" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
+        $('<div class="message new"><figure class="avatar"><img src="img/img.png" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
         setDate();
         updateScrollbar();
         i++;
